40e7f3282123ef56db6c967c00e8a91987633d6c
similola <similola-a@hotmail.com>
2016-10-22 15:49:36 +0100

    enter page with link to video created

--- js/logic.js
+++ js/logic.js
@@ -1,37 +1,78 @@
 function getVal(){
   console.log('hello');
-  var val = $('.description:checked').val();
+  var val = $('input[name=description]:checked').val();
+  console.log(val);
   var fname = $('#fname').val();
+  console.log(fname);
   createPost(fname, val);
+  window.location='campaign.html?fname=' +fname + '&attr='+val;
+
+
 
 }
 
 function createPost(fname, attr){
-  var attrList = women[attr]
-  var randomIndex = genRandomNumber(attrList.length);
-  console.log(randomIndex);
-  var matchedwoman = attrList[randomIndex];
-  console.log(matchedwoman.img);
 
-
-  //console.log(matchedwoman.img);
-  // var post = {
-  //   href: 'http://www.bbc.co.uk/programmes/p01k3j50' ,
-  //     caption: 'hellloooooo whats up',
-  //       picture: matchedwoman.img,
-  //       title: matchedwoman.name,
-  //       description: 'write an interesting description',
-  //       caption: 'i am a caption',
-  //       quote: 'write an interesting quote',
-  //       hashtag: matchedwoman.hashtag
-  // }
-
-  //console.log(post);
+  var attrList = women[attr];
+  var matchedwoman = attrList[0];
+  console.log(matchedwoman.img.valueOf());
 
 
 }
 
-function genRandomNumber(maxIndex){
-  return Math.floor((Math.random() * maxIndex) + 1);
+var women = {
 
+  'Defiant' : [
+
+    {
+      name: 'Michelle Obama',
+      img:'images/michelleobama.png',
+      quote:'When they go low, we go high.'
+    }
+  ]
+  ,
+
+  'Smart': [
+    {
+      name: 'Hilary Clinton' ,
+      img:'images/hilary.png',
+      quote:'Always aim high, work hard and care deeply about what you believe in.'
+
+    }
+  ],
+
+  'Brave': [
+    {
+      name: 'Lady Gaga',
+      img: 'ladygaga.png',
+      quote: 'I was born this way.'
+
+    }
+
+
+  ],
+
+  'Strong': [
+    {
+      name: 'Oprah',
+      img: 'oprah.png',
+      quote: 'Think like a Queen. A queen is not afraid to fail.'
+
+    }
+
+  ]
+
+
+}
+
+function getParameterByName(name, url) {
+  if (!url) {
+    url = window.location.href;
+  }
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
