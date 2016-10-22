db334c91e4371727784de3260f95894cfba71dff
Olubunmi Aworanti <olubunmi.aworanti@gmail.com>
2016-10-22 12:49:20 +0100

    beginning of merging changes

---
+++ women.js
@@ -0,0 +1,69 @@
+var women = {
+	
+	'defiant' : [
+
+		{
+			name: 'Michelle Obama', 
+			img:'images/michelleobama.png',
+			hashtag:'#IamLikeTheFirstLady' 
+		},
+
+		{
+			name: 'Michelle Obama', 
+			img:'images/michelleobama.png',
+			hashtag:'#IamLikeTheFirstLady' 
+		}
+	]
+	,
+
+	'powerful': [
+		{
+			name: 'Hilary Clinton' , 
+			img:'images/hilary.png',
+			hashtag:'#IamLikeHilary'
+		},
+		{
+			name: 'Hilary Clinton' , 
+			img:'images/hilary.png',
+			hashtag:'#IamLikeHilary'
+		},
+	],
+
+	'sexy': [
+		{
+			name: 'Lady Gaga',
+			img: 'ladygaga.png',
+			hashtag: '#IamLikeGaga'
+
+		},
+		{
+
+			name: 'Lady Gaga',
+			img: 'ladygaga.png',
+			hashtag: '#IamLikeGaga'
+
+		}
+				
+
+	],
+
+	'strong': [
+				{
+					name: 'Oprah',
+					img: 'oprah.png',
+					hashtag: '#IamLikeOprah'
+
+				},
+				{
+				name: 'Oprah',
+				img: 'oprah.png',
+				hashtag: '#IamLikeOprah'
+				}
+				
+			]
+
+
+
+
+	
+}
